00e92e58f25963d7dd0cb28da0f3311602e4f53d
pungis <filip.bsl@gmail.com>
2018-02-05 00:05:04 +0100

    star active

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,17 @@
+import 'jquery';
+
+
+
+
+
+
 require ('../css/style.scss');
 
 
+$( document ).ready(function() {
+  $('.cls-1').on('click', function() {
+    $(this).toggleClass("active");
+});
 
+});
 
